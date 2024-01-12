28fab5175a5b94108525def0a90425d12225dce6
Ly Nguyen <lynguyen4569@gmail.com>
2024-01-12 12:27:14 +0700

    add soundtrack

--- app/src/main/java/com/example/braintrainapp/MainActivity.kt
+++ app/src/main/java/com/example/braintrainapp/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.braintrainapp
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
 class MainActivity : ComponentActivity() {
+     lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        mediaPlayer = MediaPlayer.create(this, R.raw.background)
+        mediaPlayer.isLooping = true // Set looping
+        mediaPlayer.start() // Start playing
+
             setContent {
                 MyApp()
             }
-        }
     }
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release() // Release the media player on destroy
+    }
+
 }
